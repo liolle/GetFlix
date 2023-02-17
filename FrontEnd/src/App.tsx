eec030c79b27bd891052ab7b72e383e2a6e76eab
Bilal Lamrani <bilal-lamrani@hotmail.com>
2023-02-17 15:27:36 +0100

    commitTest

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router } from "react-router-dom"
-
-
 import Sidebar from './Components/Sidebar/Sidebar'
 import {routes} from './routes'
 
@@ -11,19 +9,15 @@ import {routes} from './routes'
 const App: React.FunctionComponent = () => {
   return (
   
-    
     <>
-     
-
       <Router> 
 
-        <Sidebar/>
-         
+      <Sidebar/> 
           
         {routes()}
         
       </Router> 
-     
+      
     </>
   )
 }
