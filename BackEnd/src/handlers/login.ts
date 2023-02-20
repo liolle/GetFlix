2373cd6f483fe264ed197c243741dd5162263c7e
liolle <laurentlionel20@hotmail.com>
2023-02-20 01:19:03 +0100

    first deploy

--- BackEnd/src/handlers/login.ts
+++ BackEnd/src/handlers/login.ts
@@ -125,7 +125,7 @@ const login = async (req: Request, res: Response): Promise<void> =>{
             tokenConnect(dbRes[0]['pwd'],req,res)
             
         } catch (error) {
-            
+            res.status(400).json({message: "Wrong Token"})
         }
     }
     else {
