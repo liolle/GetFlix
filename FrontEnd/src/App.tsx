e5d3850adcb1cb451d21b900935f7f19096aa882
Bilal Lamrani <bilal-lamrani@hotmail.com>
2023-02-21 22:49:02 +0100

    Dashboard

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import { BrowserRouter as Router } from "react-router-dom"
 import Sidebar from './Components/Sidebar/Sidebar'
 import {routes} from './routes'
-
 
 
 
