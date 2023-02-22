6dd96322e2ce847e37eb8f8d04634844ceae9194
liolle <laurentlionel20@hotmail.com>
2023-02-22 11:34:27 +0100

    update doc

--- BackEnd/src/handlers/selector.ts
+++ BackEnd/src/handlers/selector.ts
@@ -20,6 +20,14 @@ const getSelection = async (req: Request, res: Response): Promise<void> =>{
 
     try {
         let dbres = await movie.findSelection(composition,keyword)
+
+        dbres.forEach((element:any) => {
+            if (element['name']) {
+                let image = "https://image.tmdb.org/t/p/w500" + element['name']
+                Object.assign(element,{image:image})
+            }
+            
+        });
         res.status(200).json(dbres)
         return 
         
@@ -27,7 +35,6 @@ const getSelection = async (req: Request, res: Response): Promise<void> =>{
         res.status(500).json({message: "Internal Server Error"})
         return
     }
-
 
 }
 
