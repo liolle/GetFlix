365459447c0cd8138ea9f858b32e40faf74614c7
liolle <laurentlionel20@hotmail.com>
2023-03-06 03:32:34 +0100

    Simple test using ts-jest    run tests (BackEnd): npm run test -w BackEnd/

--- BackEnd/src/model/verif.ts
+++ BackEnd/src/model/verif.ts
@@ -1,4 +1,5 @@
 import { timeStamp } from "console";
+import { delimiter } from "path";
 import DbConnect from "../util/dbConnect";
 
 
@@ -12,8 +13,8 @@ export class Verif extends DbConnect {
     constructor(
         email:string,
         timeout:string,
-        vToken:"",
-        type:1
+        vToken="",
+        type=1
     ) {
 
         if (!email || email.length < 1) {
@@ -34,11 +35,59 @@ export class Verif extends DbConnect {
     }
 
     //'2023-03-05 20:59:00'
-    static getTimeStamp( date = new Date()):string{
+    static getTimeStamp( date = new Date(),dateStyle='yyyy-mm-dd',delimiter="-"):string{
 
-        let timeStamp =date.toISOString().slice(0, 19).replace('T', ' ')
+        function pad(number:number) {
+            if ( number < 10 ) {
+              return '0' + number;
+            }
+            return `${number}`;
+        }
 
+        const formatDate = (y:number,m:number,d:number,dateStyle='yyyy-mm-dd'):string=>{
+
+            switch (dateStyle) {
+                case "dd-mm-yyyy":
+                    return pad(d)+delimiter+pad(m)+delimiter+pad(y)
+
+                case "dd-yyyy-mm":
+                    return pad(d)+delimiter+pad(y)+delimiter+pad(m)
+
+                case "mm-yyyy-dd":
+                    return pad(m)+delimiter+pad(y)+delimiter+pad(d)
+
+                case "mm-dd-yyyy":
+                    return pad(m)+delimiter+pad(d)+delimiter+pad(y)
+
+                case "yyyy-dd-mm":
+                    return pad(y)+delimiter+pad(d)+delimiter+pad(m)
+        
+                default:
+                    return pad(y)+delimiter+pad(m)+delimiter+pad(d)
+            }
+
+        }
+
+        let timeStamp = formatDate(date.getUTCFullYear(),
+        date.getUTCMonth() + 1,
+        date.getUTCDate(),dateStyle )
+        timeStamp += ' '
+        timeStamp += pad( date.getUTCHours())
+        timeStamp += ':'
+        timeStamp += pad( date.getUTCMinutes())
+        timeStamp += ':'
+        timeStamp += pad( date.getUTCSeconds() ) 
+        
         return timeStamp
+    }
+
+    static generateToken(len:Number) {
+        var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        var token = '';
+        for(var i = 0; i < len; i++) {
+            token += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return token;
     }
 
     async findOne(){
@@ -58,9 +107,9 @@ export class Verif extends DbConnect {
         })
         
     }
-    async create(pwd:string):Promise<boolean|string>{
+    async create(vToken:string):Promise<string>{
         
-        return new Promise<boolean>((resolve, reject) => {
+        return new Promise<string>((resolve, reject) => {
 
             // let sql = `INSERT INTO user (email,vToken,type,timeout)
             // VALUES ('${this.email}', '${this.vToken}', '${this.type}','${this.timeout}')`
@@ -73,16 +122,19 @@ export class Verif extends DbConnect {
             
             */
 
+            let timeStamp = Verif.getTimeStamp()
+
             let sql = `
             INSERT INTO verification (email,vToken,timeout,type)
-            VALUES('${this.email}','${this.vToken}',TIMESTAMP('2023-03-05 20:34:00','0:15:0'),'${this.type}');
+            VALUES('${this.email}','${vToken}',TIMESTAMP('${timeStamp}','0:15:0'),${this.type})
             `
-            
+            console.log("---HERE---")
+            console.log(sql)
             this.connection.query(sql, (err:any, rows:any, fields:any)=>{
                 if (err){
-                    reject(err['sqlMessage'])
+                    resolve(err['sqlMessage'])
                 }
-                resolve(true)
+                resolve("OK")
             })
         })
     }
--- BackEnd/src/test.ts
+++ BackEnd/src/test.ts
@@ -1,19 +1,38 @@
 import {User} from "./model/users";
 import {Movies} from "./model/movie";
+import {Verif} from "./model/verif";
 require('dotenv').config()
 
 let user = new User("test2@test.com")
 
 let movie = new Movies(-1,"","","","","action,thriller")
 
-movie.findRandom(5)
-.then((data)=>{
-    console.log(data)
+// movie.findRandom(5)
+// .then((data)=>{
+//     console.log(data)
 
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+let verif1 = new Verif(
+    "test@test.com",
+    Verif.getTimeStamp(),
+    "testToken01",
+    1
+)
+
+verif1.create("testToken01")
+.then((msg)=>{
+    
+    console.log(msg)
 })
-.catch((err)=>{
-    console.log(err)
-})
+
+
+
+
+
 
 /*
 movie.findSelection("","")
