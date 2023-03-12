2f05bbc8a025ce96b4cdfd6132b988587c3d6c71
Alex Atsoak <atsoak alex>
2023-03-12 03:23:16 +0100

    preview

--- FrontEnd/src/Components/Watch/Watch.tsx
+++ FrontEnd/src/Components/Watch/Watch.tsx
@@ -1,49 +1,69 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-interface Movie {
-  id: string;
-  title: string;
-  image: string;
-  videoLink: string;
-  description: string;
-  rating: number; // Ajout de la propriété rating
-  type: string; // Ajout de la propriété type
-}
 
-const View = () => {
-  const [movie, setMovie] = useState<Movie | null>(null);
-  const [error, setError] = useState<string | null>(null); // Ajout de la gestion d'erreur
 
-  useEffect(() => {
-    axios
-      .get('https://getflix-production-8eb4.up.railway.app/api/movies/random')
-      .then((response) => setMovie(response.data))
-      .catch((error) => setError('Une erreur est survenue lors du chargement des données')); // Gestion de l'erreur
-  }, []);
 
-  if (error) {
-    return <div>Une erreur est survenue : {error}</div>; // Affichage de l'erreur
-  }
 
-  if (!movie) {
-    return <div>Loading...</div>;
-  }
 
-  return (
-    <div className="flex">
-      <div className="w-1/3">
-        <img src={movie.image} alt={movie.title} />
+const MovieCarousel: React.FC = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+  
+    useEffect(() => {
+      axios.get<Movie[]>('https://getflix-production-8eb4.up.railway.app/api/movies/random')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
+  
+    const responsive = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+      },
+    };
+  
+    return (
+      
+      <div className='bg-black text-white mr-4 '>
+        <Card
+          responsive={responsive}
+          showDots={true}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={4000}
+        >
+          {movies.map(movie => (
+            <div key={movie.movieId} className="relative">
+              <a href={movie.videoLink}>
+                <img src={movie.image} alt={movie.title} />
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                  <button className="w-16 h-16 rounded-full bg-skyblue flex items-center justify-center ">
+                    <i className="fa fa-play text-white"></i>
+                  </button>
+                </div>
+              </a>
+              <h3>{movie.title}</h3>
+              <p>{movie.description}</p>
+            </div>
+          ))}
+        </Card>
       </div>
-      <div className="w-1/3">
-        <h2>{movie.title}</h2>
-        <p>{movie.description}</p>
-      </div>
-     
-    </div>
-  );
-};
-
-export default View;
+    );
+  };
+  
+  export default MovieCarousel
+  
 
   
