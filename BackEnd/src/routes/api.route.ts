dacaf7ad369b3d43c43ae39c03dbeb2b9e56edae
liolle <laurentlionel20@hotmail.com>
2023-03-14 04:36:52 +0100

    deploy cookies with cors test1

--- BackEnd/src/routes/api.route.ts
+++ BackEnd/src/routes/api.route.ts
@@ -5,7 +5,7 @@ import getTypes from "../handlers/types";
 import getRandom from "../handlers/random";
 
 
-// take parameter /select?type=t1,t2,...&title=x&composition=union|inter&keyword=x          
+// take parameter /select?type=t1,t2,...&title=x&composition=union|inter&keyword=x   
 router.get('/movies/select',getSelection)
 router.get('/movies/types',getTypes)
 router.get('/movies/random',getRandom)
